1f9a96ef9721e2bef3827043cdddb24881b6bcb1
sandeepshetty <sandeep.shetty@gmail.com>
2012-10-07 06:05:09 +0530

    Copy edits..

--- templates/index.html.php
+++ templates/index.html.php
@@ -29,11 +29,11 @@
 		<div  class="alert alert-info">
 			<p>Clicking on send will do the following:
 				<ol>
-					<li><code>Target</code> will be visited and an attempt will be made to <em>discover</em> an activity pingback endpoint.</li>
+					<li>An attempt will be made to discover the activity pingback endpoint for <code>Target</code>.</li>
 					<li>If an endpoint is found
 						<ul>
-							<li>and it turns out that <code>Target</code> is using <code>pingback.converspace.com</code> as its activity pingback endpoint, then it will retrieve the activity from <code>source</code> and valdate it.  All activites for a given resource will be made available at <code>http://pingback.converspace.com/activities/for/{resource}</code> where <code>{resource}</code> is the URI of the resource.</li>
-							<li>else it will send an activity pingback to the endpoint.</li>
+							<li>and it turns out that <code>Target</code> is using <code>pingback.converspace.com</code> as its activity pingback endpoint, then the activity will be retrieved from <code>source</code> and validated.  All activites for a given resource will be made available at <code>http://pingback.converspace.com/activities/for/{resource}</code> where <code>{resource}</code> is the URI of the resource.</li>
+							<li>otherwise, an activity pingback will be sent to the discovered endpoint.</li>
 						</ul>
 					</li>
 				</ol>
